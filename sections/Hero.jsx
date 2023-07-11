c93f93849227409c55ac6fdb9ca031bd3739d1cb
H-ymt <107012772+H-ymt@users.noreply.github.com>
2023-07-11 17:11:10 +0900

    add Hero

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -4,16 +4,11 @@ import styles from "../styles";
 import { motion } from "framer-motion";
 import { slideIn, staggerContainer, textVariant } from "@/utils/motion";
 
-type Props = {
-  staggerChildren: string;
-  delayChildren: string;
-};
-
 export default function Hero() {
   return (
     <section>
       <motion.div
-        // variants={staggerContainer}
+        variants={staggerContainer}
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
@@ -33,6 +28,11 @@ export default function Hero() {
           <div className={styles.heroDText} />
           <h1 className={styles.heroHeading}>Ness</h1>
         </motion.div>
+
+        <motion.div
+          variants={slideIn("right", "tween", 0.2, 1)}
+          className="relative w-full md:-mt-[20px] -mt-[12px]"
+        ></motion.div>
       </div>
     </section>
   );
